2f6b208cd92cc2d641093048a4eee17aa388b90e
Arifur Rahman Sujon <arifurrahmansujon27@gmail.com>
2023-08-30 23:05:23 +0600

    init commit

---
+++ src/test/java/com/example/graphql_demo/GraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphql_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
